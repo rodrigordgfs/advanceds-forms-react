02b0c6c58bc66bee56b6ea9b34c3e6213abbdec4
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-04-06 19:46:39 -0300

    Feature: Transformação dos dados

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,24 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const createUserFormSchema = z.object({
+  name: z
+    .string()
+    .nonempty("O nome é obrigatório")
+    .transform((v) => {
+      return v
+        .trim()
+        .split(" ")
+        .map((v) => v[0].toLocaleUpperCase().concat(v.substring(1)))
+        .join(" ");
+    }),
   email: z
     .string()
     .nonempty("O e-mail é obrigatório")
-    .email("O e-mail é inválido"),
+    .email("O e-mail é inválido")
+    .toLowerCase()
+    .refine((v) => {
+      return v.endsWith("@rocketseat.com.br");
+    }, "O e-mail deve ser da Rocketseat"),
   password: z.string().min(6, "A senha deve ter no mínimo 6 caracteres"),
 });
 
@@ -33,6 +47,18 @@ export default function App() {
         className="flex flex-col gap-4 w-full max-w-xs"
         onSubmit={handleSubmit(createUser)}
       >
+        <div className="flex flex-col gap-1">
+          <label htmlFor="name">Nome</label>
+          <input
+            className="border bg-zinc-800 border-zinc-600 shadow-sm rounded h-10 px-3"
+            type="name"
+            {...register("name")}
+          />
+          {errors.name && (
+            <span className="text-xs text-red-500">{errors.name.message}</span>
+          )}
+        </div>
+
         <div className="flex flex-col gap-1">
           <label htmlFor="email">E-mail</label>
           <input
@@ -60,7 +86,7 @@ export default function App() {
         </div>
 
         <input
-          className="bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600"
+          className="bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600 cursor-pointer"
           type="submit"
           value="Salvar"
         />
